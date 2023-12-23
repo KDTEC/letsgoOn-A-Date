9e655c8a7815bafee391cf871d76646326d565e2
Kshitij Dhawan <kshitij.dhawan@Dmmedcity-ITs-MacBook-Pro-2.local>
2023-12-23 20:29:43 +0530

    Updated the code

--- server.js
+++ server.js
@@ -7,34 +7,32 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public')); // Serve static files from the 'public' directory
 
-
 const transporter = nodemailer.createTransport({
-  service: 'gmail', 
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-app.post('/send-email', (req, res) => {
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: process.env.EMAIL_RECEIVER,
-    subject: 'She said YES!',
-    text: 'Congratulations, she said YES!',
-  };
+app.post('/send-email', async (req, res) => {
+  try {
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_RECEIVER,
+      subject: 'She said YES!',
+      text: 'Congratulations, she said YES!',
+    };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send('Error sending email');
-    } else {
-      console.log('Email sent: ' + info.response);
-      res.status(200).send('Email sent successfully');
-    }
-  });
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent: ' + info.response);
+    res.status(200).send('Email sent successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error sending email');
+  }
 });
 
 app.listen(4000, () => {
-  console.log('Server started on port 3000');
+  console.log('Server started on port 4000');
 });
